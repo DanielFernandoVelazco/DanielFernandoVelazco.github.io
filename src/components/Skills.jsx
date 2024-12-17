422eebfb1e31e771d49b5df4f336ad9ae88a5e11
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2024-12-17 13:21:16 -0500

    Se actializa las habilidades

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,9 +4,10 @@ const Skills = () => (
     <section style={{ padding: '20px' }}>
         <h2>Habilidades</h2>
         <ul>
-            <li>HTML, CSS, JavaScript</li>
-            <li>React, Redux</li>
+            <li>HTML, CSS, JavaScript, Java</li>
+            <li>Postgres, MySQL, MongoDB</li>
             <li>Node.js, Express</li>
+            <li>NestJS, React, Spring, Swagger, Docker</li>
             <li>Git, GitHub</li>
         </ul>
     </section>

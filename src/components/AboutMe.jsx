5f536f0eb8d0a31183d8536d6eb30431938f5bef
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2024-12-17 13:19:26 -0500

    Se modifica la introduccion

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,8 +4,10 @@ const AboutMe = () => (
     <section style={{ padding: '20px' }}>
         <h2>Sobre Mí</h2>
         <p>
-            Soy un desarrollador web apasionado con experiencia en la creación de aplicaciones modernas y
-            escalables. Me encanta resolver problemas y aprender nuevas tecnologías.
+        <b>INGENIERO DE SISTEMAS</b>, innovador, apasionado por la tecnología, creativo con capacidad de aprender constantemente y razonamiento lógico, manejo de programas como JavaScript, Java y Python, además sólidos conocimientos en la instalación y puesta en marcha de redes LAN.
+        </p>
+        <p>
+        Responsable, creativo, con iniciativa y puntualidad, asumo con responsabilidad los retos y metas que la organización me plantee; adaptabilidad al cambio, manejo de personal, facilidad para trabajar en equipo, en condiciones de alta presión, así como para resolver problemas eficientemente y lograr las metas de productividad trazadas por la empresa y mi grupo de trabajo.
         </p>
     </section>
 );

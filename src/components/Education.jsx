9ced9240bcfb9177af300b6396c5506fc8f319c8
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2024-12-17 13:20:09 -0500

    Se actializa la educacion

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,7 +5,28 @@ const Education = () => (
         <h2>Educación</h2>
         <ul>
             <li>
-                <strong>Ingeniería en Sistemas - Universidad Ejemplo</strong> (2015 - 2020)
+                <p>
+                    <strong>TECNÓLOGO - ANALISIS Y DESARROLLO DE SOFTWARE</strong> - SERVICIO NACIONAL DE APRENDIZAJE (SENA) - Actualmente
+                </p>
+                <p>
+                    Virtual
+                </p>
+            </li>
+            <li>
+                <p>
+                    <strong>PROFESIONAL – INGENIERÍA DE SISTEMAS</strong> - FUNDACIÓN DE EDUCACIÓN SUPERIOR SAN JOSÉ - USANJOSE (USANJOSE) - 2023
+                </p>
+                <p>
+                    Bogotá, Colombia
+                </p>
+            </li>
+            <li>
+                <p>
+                    <strong>TECNÓLOGO – SISTEMAS DE INFORMACIÓN</strong> - FUNDACIÓN DE EDUCACIÓN SUPERIOR SAN JOSÉ - USANJOSE (USANJOSE) - 2022
+                </p>
+                <p>
+                    Bogotá, Colombia
+                </p>
             </li>
         </ul>
     </section>

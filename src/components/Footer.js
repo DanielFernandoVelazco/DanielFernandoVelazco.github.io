9fe582f6b5cd105ca87261b99ba16f4d4bc198c4
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2024-12-19 11:58:43 -0500

    Se modifica el Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <footer>
-        <p>© 2024 Mi Nombre. <a href="https://github.com/mi-usuario">GitHub</a></p>
+        <p>© 2024 Daniel Fernando Velazco Caceres v1.0.0 <a href="https://github.com/mi-usuario">GitHub</a></p>
     </footer>
 );
 

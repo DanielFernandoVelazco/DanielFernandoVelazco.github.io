e4dc5e0f8e48b556a48f209e79ff5cd35412fdde
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-06 17:31:04 -0500

    Actualización de CV con nuevo diseño

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <footer>
-        <p>© 2024 Daniel Fernando Velazco Caceres v1.0.0 <a href="https://github.com/mi-usuario">GitHub</a></p>
-    </footer>
-);
-
-export default Footer;
